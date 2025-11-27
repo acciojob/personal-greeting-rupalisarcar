f093d96391d1215b110c0cc83c6154b27579fbc7
rupalisarcar <rupalisarcar@gmail.com>
2025-11-27 09:52:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,26 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+   const [inputValue, setInputValue] = useState('')
+    const takeInput= (el)=>{
+      console.log('keyDown',el)
+      console.log('keyDown',el.key)
+      
+    }
+
+    const handleChange=(event)=>{
+      setInputValue(event.target.value)  
+    }
+
+   return (
+      <div>
+         <label>Enter name
+        <input type='text' value={inputValue} onChange={handleChange} onKeyDown={takeInput} />
+      </label>
+       {inputValue && <p>Hello {inputValue}!</p>}
+      </>
+      </div>
+    )
 }
 
 export default App
